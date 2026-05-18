81b61caeca0dba473a74fbe97b74a22d8c6fa850
ANA CLARA SERRANO DOS SANTOS <ana.cssantos187@senacsp.edu.br>
2026-05-18 20:35:23 -0300

    atualização

--- API/construindoapi.js
+++ API/construindoapi.js
@@ -32,8 +32,8 @@ const server = http.createServer((req, res) =>{
  
 
     //Liberação do CORS :
-    res.setHeader ("cess-Control-Allow-Origin", "*");
-    res.setHeader ("Acess-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Acess-Control-Allow-Origin", "*");
+    res.setHeader("Acess-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Acess-Control-Allow-Headers", "Content-Type");
 
     if(metodo ==="OPTIONS") {
